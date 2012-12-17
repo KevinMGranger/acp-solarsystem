3598655ff73b61bca57b9e36d5ecfa5d4f53e9f6
Kevin Granger <kmg2728@rit.edu>
2012-12-17 17:39:21 -0500

    Initial commit. Program does not run yet.
    Just .gitignore, some notes in the program and the start of some
    documentation.

---
+++ solar_system.m
@@ -0,0 +1,32 @@
+function solar_system(ending_time, time_step)
+
+DESCRIPTION
+    Simulate the motion of 
+    
+ARGUMENTS
+    ending_time : the duration of the simulation, in DAYS
+
+    timestep : the timestep used in the numerical integration, in SECONDS
+
+    
+
+    
+    
+    
+    
+% NOTES: run program for 1 year, earth should be at starting point! or use
+% the website to find out where it should be exactly
+    
+    
+% Helpful Constants
+
+AU=149597870.691E3; % meters
+
+% Set up initial data
+
+NUM_BODIES = ; %number of astronomical bodies to compute for.
+
+
+% Body 1: Sun (SOL)
+% Body 2: Earth
+% Body 3: Jupiter
